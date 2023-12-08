2004dc7a73f6840c6d00879fa76ae8f330049baa
liuzhao.tang@svfactory.com.cn <ssFp@3985>
2023-12-08 18:06:47 +0800

    update

--- 02_data_generation.py
+++ 02_data_generation.py
@@ -34,60 +34,57 @@ agg_dict = {'down_time': ['sum', 'mean', 'median', 'max', 'min', 'count'],
 
 
 def data_generation(paths):
-    for path in tqdm(paths):
-        df = pd.read_csv(path)
+    df = pd.read_csv(path)
+    # add new columns
+    # add time gap
+    df = map_period(df, 'down_time', [5, 10])
 
-        # add new columns
-        # add time gap
-        df = map_period(df, 'down_time', [5, 10])
+    # add pressed button type
+    df['pressed_button_type'] = df['down_event'].apply(map_button)
 
-        # add pressed button type
-        df['pressed_button_type'] = df['down_event'].apply(map_button)
+    # add is_mistake_touch
+    df['is_mistake_touch'] = df['down_event'].apply(lambda x: 1 if x in mistake_touch + F_keys else 0)
 
-        # add is_mistake_touch
-        df['is_mistake_touch'] = df['down_event'].apply(lambda x: 1 if x in mistake_touch + F_keys else 0)
+    # add word_length
+    # df['word_length'] = (df['cursor_position'] + 1e-10) / (df['word_count'] + 1e-10)
 
-        # add word_length
-        # df['word_length'] = (df['cursor_position'] + 1e-10) / (df['word_count'] + 1e-10)
+    # change column value
+    df['activity'] = df['activity'].apply(lambda x: 'Move' if x.startswith('Move') else x)
 
-        # change column value
-        df['activity'] = df['activity'].apply(lambda x: 'Move' if x.startswith('Move') else x)
+    for column in ['down_event', 'up_event']:
+        conditions = [df[column].isin(F_keys), df[column].isin(mouse_operation), df[column].isin(feature_key_operation),
+                      df[column] == 'q', df[column].isin(signs), df[column].isin(numbers)]
+        categories = ['F_keys', 'mouse', 'feature_key', 'input', 'signs', 'numbers']
+        df[column] = np.select(conditions, categories, default='other')
 
-        for column in ['down_event', 'up_event']:
-            conditions = [df[column].isin(F_keys), df[column].isin(mouse_operation), df[column].isin(feature_key_operation),
-                          df[column] == 'q', df[column].isin(signs), df[column].isin(numbers)]
-            categories = ['F_keys', 'mouse', 'feature_key', 'input', 'signs', 'numbers']
-            df[column] = np.select(conditions, categories, default='other')
+    # group by id
+    group_by_id_df = df.groupby(['id']).agg(agg_dict)
+    group_by_id_df.columns = [f'{col[0]}_{col[1]}' for col in group_by_id_df.columns]
+    group_by_id_df['word_length'] = group_by_id_df['cursor_position_max'] / group_by_id_df['word_count_max'] - 2
+    group_by_id_df['word_length'] = group_by_id_df['word_length'].replace([np.inf, -np.inf], 0)
+    merged_df = group_by_id_df.copy().reset_index()
+    # is_nan_check(group_by_id_df)
 
-        # group by id
-        group_by_id_df = df.groupby(['id']).agg(agg_dict)
-        group_by_id_df.columns = [f'{col[0]}_{col[1]}' for col in group_by_id_df.columns]
-        group_by_id_df['word_length'] = group_by_id_df['cursor_position_max'] / group_by_id_df['word_count_max'] - 2
-        merged_df = group_by_id_df.copy().reset_index()
-        # is_nan_check(group_by_id_df)
+    # group by id and other columns
+    for column in df.columns:
+        if column not in ['id', 'event_id', 'up_time', 'down_time', 'action_time', 'cursor_position', 'word_count']:
+            agg_dict_ = agg_dict.copy()
+            del agg_dict_[column]
+            group_df = df.groupby(['id', column]).agg(agg_dict_)
+            group_df.columns = [f'{col[0]}_{col[1]}' for col in group_df.columns]
+            group_df['word_length'] = group_df['cursor_position_max'] / group_df['word_count_max'] - 2
+            group_df['word_length'] = group_df['word_length'].replace([np.inf, -np.inf], 0)
+            group_df = group_df.reset_index().pivot_table(index='id', columns=column, values=list(group_df.columns)[2:],
+                                                          fill_value=0)
+            group_df.columns = [f'{col[0]}_{col[1]}' for col in group_df.columns if col != 'id']
+            merged_df = merged_df.merge(group_df, on='id', how='left', suffixes=('', f'_{column}'))
+        time.sleep(1)
 
-        # group by id and other columns
-        for column in df.columns:
-            if column not in ['id', 'event_id', 'up_time', 'down_time', 'action_time', 'cursor_position', 'word_count']:
-                agg_dict_ = agg_dict.copy()
-                del agg_dict_[column]
-                group_df = df.groupby(['id', column]).agg(agg_dict_)
-                group_df.columns = [f'{col[0]}_{col[1]}' for col in group_df.columns]
-                group_df['word_length'] = group_df['cursor_position_max'] / group_df['word_count_max'] - 2
-                group_df = group_df.reset_index().pivot_table(index='id', columns=column, values=list(group_df.columns)[2:],
-                                                              fill_value=0)
-                group_df.columns = [f'{col[0]}_{col[1]}' for col in group_df.columns if col != 'id']
-                merged_df = merged_df.merge(group_df, on='id', how='left', suffixes=('', f'_{column}'))
-            time.sleep(1)
-
-        # save file
-        merged_df.to_csv(path.replace('train_logs', 'engineered_train_logs'), index=False)
+    # save file
+    merged_df.to_csv(path.replace('train_logs', 'engineered_train_logs'), index=False)
 
 if __name__ == '__main__':
-    paths = []
-    for dirname, _, filenames in os.walk('datasets'):
-        for filename in filenames:
-            paths.append(os.path.join(dirname, filename))
-
-    data_generation(paths)
+    for index in range(9):
+        path = f'datasets/train_logs_{index}.csv'
+        data_generation(path)
 
--- 03_quick_feature_filter.py
+++ 03_quick_feature_filter.py
@@ -1,25 +1,32 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 if __name__ == '__main__':
     result = pd.DataFrame()
 
     target_df = pd.read_csv('data/train_scores.csv')
     for index in range(9):
+        print('index: ', index)
         path = f'datasets/engineered_train_logs_{index}.csv'
         feature_df = pd.read_csv(path)
         ids = feature_df['id'].unique().tolist()
-        target_df = target_df[target_df['id'].isin(ids)]
 
         X = feature_df.drop(['id'], axis=1)
-        Y = target_df.drop(['id'], axis=1)
+        Y = target_df[target_df['id'].isin(ids)]['score']
+
+        print('X shape: ', X.shape)
+        print('Y shape: ', Y.shape)
 
         # split data set
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
 
-        rf = RandomForestClassifier(n_estimators=200)
+        scaler = StandardScaler()
+        X_train = scaler.fit_transform(X_train)
+
+        rf = RandomForestRegressor(n_estimators=200)
         rf.fit(X_train, Y_train)
 
         # get importance
